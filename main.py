e878f12e83bc6dfc275f56862bacfc0305c236d0
Kyu Lee <kwooler@gmail.com>
2022-10-23 18:13:47 -0700

    Lab14

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import random
+def fibo(N):
+
+   ##################################################
+   # make your code
+   ##################################################
 
 
-def findEvenNumber(numbers):
-    ##################################################
-    # Make your code
-    ##################################################
-    return
-
-
-numbers = [random.randint(0, 100) for i in range(10)]
-print('Original list', numbers)
-gen = findEvenNumber(numbers)
-
-for i in gen:
-    print(i, end=' ')
+N = 16
+gen = fibo(N)
+for v in gen:
+    print(v, end=' ')
+print()
--- main_test.py
+++ main_test.py
@@ -4,18 +4,53 @@ import main
 
 def test_main():
 
-    numbers = [random.randint(0, 100) for i in range(10)]
-    print('Original list', numbers)
-    evenlst = [v for v in numbers if v % 2 == 0]
-    cnteven = len(evenlst)
-
-    gen = main.findEvenNumber(numbers)
+    N = 0
+    gen = main.fibo(N)
     resultlst = list(gen)
-    print('The number of elements in your result', len(resultlst))
-    print(resultlst)
-    for i in gen:
-        print(i, end=' ')
+    print('Your result: ', end=' ')
+    for v in resultlst:
+        print(v, end=' ')
+    print()
 
-    assert cnteven == len(
-        resultlst), "The number of even elements are incorrect"
-    assert cnteven == 7, "The number of even elements are incorrect."
+    assert len(resultlst) == 1, "Wrong number of elements"
+    assert resultlst[0] == 0, "Invalid value"
+
+    N = 5
+    gen = main.fibo(N)
+    resultlst = list(gen)
+    print('Your result: ', end=' ')
+    for v in resultlst:
+        print(v, end=' ')
+    print()
+
+    assert len(resultlst) == 6, "Wrong number of elements"
+    assert resultlst[0] == 0, "Invalid value "
+    assert resultlst[1] == 1, "Invalid value "
+    assert resultlst[2] == 1, "Invalid value "
+    assert resultlst[3] == 2, "Invalid value "
+    assert resultlst[4] == 3, "Invalid value "
+    assert resultlst[5] == 5, "Invalid value "
+
+
+def test_fibo2():
+
+    N = 10
+    gen = main.fibo(N)
+    resultlst = list(gen)
+    print('Your result: ', end=' ')
+    for v in resultlst:
+        print(v, end=' ')
+    print()
+
+    assert len(resultlst) == 11, "Wrong number of elements"
+    assert resultlst[0] == 0, "Invalid value "
+    assert resultlst[1] == 1, "Invalid value "
+    assert resultlst[2] == 1, "Invalid value "
+    assert resultlst[3] == 2, "Invalid value "
+    assert resultlst[4] == 3, "Invalid value "
+    assert resultlst[5] == 5, "Invalid value "
+    assert resultlst[6] == 8, "Invalid value "
+    assert resultlst[7] == 13, "Invalid value "
+    assert resultlst[8] == 21, "Invalid value "
+    assert resultlst[9] == 34, "Invalid value "
+    assert resultlst[10] == 55, "Invalid value "
