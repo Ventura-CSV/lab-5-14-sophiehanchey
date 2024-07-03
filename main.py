9e690f1d78ad01ffcf8126fe879b6cbafad315b4
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 19:21:24 -0700

    error yielding n0 and n1

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def fibo(N):
     n1 = 0
     n2 = 1
+    yield n1, n2
     c = 0
     while c <= N:
-        yield n1 + n2
+        n3 = n1 + n2
+        yield n3
         n1 = n2
-        n2 = n1 + n2
+        n2 = n3
         c += 1
-
 
 def main():
     N = 16
