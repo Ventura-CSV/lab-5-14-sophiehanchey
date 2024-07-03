6364c211fd25e1c075cc0b31fbb53b6a6821824c
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 19:25:15 -0700

    returnign wrong number of values

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def fibo(N):
+    # define n1 and n2
     n1 = 0
     n2 = 1
-    yield n1, n2
+    
+    #yield n1 and n2
+    yield n1
+    yield n2
+    
+    # generate remainder of fibonacci sequence
     c = 0
-    while c <= N:
+    while c <= N-2:
         n3 = n1 + n2
         yield n3
+        
+        # update values
         n1 = n2
         n2 = n3
         c += 1
